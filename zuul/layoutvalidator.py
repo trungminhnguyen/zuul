d1403451e218a481e405755e285edd249e3cbf28
Gregory Haynes <greg@greghaynes.net>
2015-04-22 20:38:06 -0700

    (WIP) Support GitHub PR webhooks
    Co-Authored-By: Wayne Warren (waynr+launchpad@sdf.org)
    Co-Authored-By: Jan Hruban (jan.hruban@gooddata.com)
    
    Change-Id: I2713c5d19326213539689e9d822831a393b2bf19

--- zuul/layoutvalidator.py
+++ zuul/layoutvalidator.py
@@ -26,6 +26,7 @@ def toList(x):
 
 
 class LayoutSchema(object):
+
     include = {'python-file': str}
     includes = [include]
 
@@ -160,6 +161,7 @@ class LayoutSchema(object):
         connection_drivers = {
             'trigger': {
                 'gerrit': 'zuul.trigger.gerrit',
+                'github': 'zuul.trigger.github',
             },
             'reporter': {
                 'gerrit': 'zuul.reporter.gerrit',
@@ -277,6 +279,7 @@ class LayoutSchema(object):
 
 
 class LayoutValidator(object):
+
     def checkDuplicateNames(self, data, path):
         items = []
         for i, item in enumerate(data):

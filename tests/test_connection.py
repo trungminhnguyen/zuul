006f4688de1c3e3e90024a633f1fff9d811435f3
Gregory Haynes <greg@greghaynes.net>
2015-04-22 20:38:06 -0700

    (WIP) Support GitHub PR webhooks
    Co-Authored-By: Wayne Warren (waynr+launchpad@sdf.org)
    Co-Authored-By: Jan Hruban (jan.hruban@gooddata.com)
    
    Change-Id: I2713c5d19326213539689e9d822831a393b2bf19

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -28,6 +28,14 @@ class TestGerritConnection(testtools.TestCase):
                          zuul.connection.gerrit.GerritConnection.driver_name)
 
 
+class TestGitHubConnection(testtools.TestCase):
+    log = logging.getLogger("zuul.test_connection")
+
+    def test_driver_name(self):
+        self.assertEqual('github',
+                         zuul.connection.github.GithubConnection.driver_name)
+
+
 class TestConnections(ZuulTestCase):
     def setup_config(self, config_file='zuul-connections-same-gerrit.conf'):
         super(TestConnections, self).setup_config(config_file)

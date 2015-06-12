5aac0fa5059abd5b8c49cbe981fb90bf34987e8d
Wayne <wayne@puppetlabs.com>
2015-06-11 17:14:13 -0700

    (WIP) Add 'push' and 'tag' github webhook events.
    Co-Authored-By: Jan Hruban (jan.hruban@gooddata.com)
    
    Change-Id: Ie7ea0f4e264beac530d8eeda13919342a9c9eb58

--- tests/test_github.py
+++ tests/test_github.py
@@ -16,7 +16,7 @@ import logging
 import re
 from testtools.matchers import MatchesRegex
 
-from tests.base import ZuulTestCase
+from tests.base import ZuulTestCase, random_sha1
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-32s '
@@ -63,3 +63,46 @@ class TestGithub(ZuulTestCase):
             re.DOTALL
         ))
         self.assertEqual(1, len(pr.comments))
+
+    def test_tag_event(self):
+        self.worker.hold_jobs_in_build = True
+
+        sha = random_sha1()
+        self.fake_github.emitEvent(
+            self.fake_github.getTagEvent('org/project', 'newtag', sha))
+        self.waitUntilSettled()
+
+        build_params = self.builds[0].parameters
+        self.assertEqual('refs/tags/newtag', build_params['ZUUL_REF'])
+        self.assertEqual('00000000000000000000000000000000',
+                         build_params['ZUUL_OLDREV'])
+        self.assertEqual(sha, build_params['ZUUL_NEWREV'])
+
+        self.worker.hold_jobs_in_build = False
+        self.worker.release()
+        self.waitUntilSettled()
+
+        self.assertEqual('SUCCESS',
+                         self.getJobFromHistory('project-tag').result)
+
+    def test_push_event(self):
+        self.worker.hold_jobs_in_build = True
+
+        old_sha = random_sha1()
+        new_sha = random_sha1()
+        self.fake_github.emitEvent(
+            self.fake_github.getPushEvent('org/project', 'master',
+                                          old_sha, new_sha))
+        self.waitUntilSettled()
+
+        build_params = self.builds[0].parameters
+        self.assertEqual('refs/heads/master', build_params['ZUUL_REF'])
+        self.assertEqual(old_sha, build_params['ZUUL_OLDREV'])
+        self.assertEqual(new_sha, build_params['ZUUL_NEWREV'])
+
+        self.worker.hold_jobs_in_build = False
+        self.worker.release()
+        self.waitUntilSettled()
+
+        self.assertEqual('SUCCESS',
+                         self.getJobFromHistory('project-post').result)

3e880f614f89ec38dc56f6d94de31073bde0f6a4
Jan Hruban <jan.hruban@gooddata.com>
2015-10-23 16:47:01 +0200

    Make the string representation of change transparent
    Implement __str__ method for the Changeish and utilize it when setting a
    job description.
    
    Change-Id: Icc2c0eb7179a3c8fdb862f89881b0c4b94c307c3

--- zuul/scheduler.py
+++ zuul/scheduler.py
@@ -1742,7 +1742,7 @@ class BasePipelineManager(object):
 
         for change in build.build_set.other_changes:
             concurrent_changes += '<li><a href="{change.url}">\
-              {change.number},{change.patchset}</a></li>'.format(
+              {change}</a></li>'.format(
                 change=change)
 
         change = build.build_set.item.change
@@ -1789,7 +1789,7 @@ class BasePipelineManager(object):
             ret = """\
 <p>
   Triggered by change:
-    <a href="{change.url}">{change.number},{change.patchset}</a><br/>
+    <a href="{change.url}">{change}</a><br/>
   Branch: <b>{change.branch}</b><br/>
   Pipeline: <b>{self.pipeline.name}</b>
 </p>"""

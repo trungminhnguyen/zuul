blurrycontour/zuul-gerrit

Add base class for reporters
and test the all reporters adhere to the set contract.

Also standardise the reporter (triggers+sources to come) class names
to NameReporter.

This will make it easier to do more reporters in the future and also
add the possibility of loading reporters dynamically.

Change-Id: Ie67537c44bbb0dc5aa2a4708ffb5d381ce7e80fc

tests/test_scheduler.py
@@ -2636,23 +2636,23 @@ For CI problems and help debugging, contact ci@example.org"""
 
         self.assertTrue(isinstance(
             self.sched.layout.pipelines['check'].merge_failure_actions[0].
-            reporter, zuul.reporter.gerrit.Reporter))
+            reporter, zuul.reporter.gerrit.GerritReporter))
 
         self.assertTrue(
             (
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[0].reporter,
-                           zuul.reporter.smtp.Reporter) and
+                           zuul.reporter.smtp.SMTPReporter) and
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[1].reporter,
-                           zuul.reporter.gerrit.Reporter)
+                           zuul.reporter.gerrit.GerritReporter)
             ) or (
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[0].reporter,
-                           zuul.reporter.gerrit.Reporter) and
+                           zuul.reporter.gerrit.SMTPReporter) and
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[1].reporter,
-                           zuul.reporter.smtp.Reporter)
+                           zuul.reporter.smtp.GerritReporter)
             )
         )
 

trungminhnguyen/zuul

Co-Authored-By: Gregory Haynes <greg@greghaynes.net>

tests/test_scheduler.py
@@ -3313,23 +3313,23 @@ For CI problems and help debugging, contact ci@example.org"""
 
         self.assertTrue(isinstance(
             self.sched.layout.pipelines['check'].merge_failure_actions[0].
-            reporter, zuul.reporter.gerrit.Reporter))
+            reporter, zuul.reporter.gerrit.GerritReporter))
 
         self.assertTrue(
             (
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[0].reporter,
-                           zuul.reporter.smtp.Reporter) and
+                           zuul.reporter.smtp.SMTPReporter) and
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[1].reporter,
-                           zuul.reporter.gerrit.Reporter)
+                           zuul.reporter.gerrit.GerritReporter)
             ) or (
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[0].reporter,
-                           zuul.reporter.gerrit.Reporter) and
+                           zuul.reporter.gerrit.GerritReporter) and
                 isinstance(self.sched.layout.pipelines['gate'].
                            merge_failure_actions[1].reporter,
-                           zuul.reporter.smtp.Reporter)
+                           zuul.reporter.smtp.SMTPReporter)
             )
         )
 
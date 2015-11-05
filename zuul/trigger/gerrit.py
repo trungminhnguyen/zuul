blurrycontour/zuul-gerrit

Configure triggers dynamically
Move EventFilter configuration into the triggers to allow dynamic
inclusion of triggers rather than specifying each one in the sechduler.

Conflicts:
	zuul/scheduler.py

Change-Id: I4ed345058ff1ffdd662fafa854e36782cc7f047b

zuul/trigger/gerrit.py
@@ -16,7 +16,7 @@ import logging
 import threading
 import time
 import voluptuous
-from zuul.model import TriggerEvent
+from zuul.model import EventFilter, TriggerEvent
 from zuul.trigger import BaseTrigger
 
 
@@ -135,6 +135,51 @@ class GerritTrigger(BaseTrigger):
     def stop(self):
         self.gerrit_connector.stop()
         self.gerrit_connector.join()
+
+    def getEventFilters(self, trigger_conf):
+        def toList(item):
+            if not item:
+                return []
+            if isinstance(item, list):
+                return item
+            return [item]
+
+        efilters = []
+        if 'gerrit' in trigger_conf:
+            for trigger in toList(trigger_conf['gerrit']):
+                approvals = {}
+                for approval_dict in toList(trigger.get('approval')):
+                    for k, v in approval_dict.items():
+                        approvals[k] = v
+                # Backwards compat for *_filter versions of these args
+                comments = toList(trigger.get('comment'))
+                if not comments:
+                    comments = toList(trigger.get('comment_filter'))
+                emails = toList(trigger.get('email'))
+                if not emails:
+                    emails = toList(trigger.get('email_filter'))
+                usernames = toList(trigger.get('username'))
+                if not usernames:
+                    usernames = toList(trigger.get('username_filter'))
+                f = EventFilter(
+                    trigger=self,
+                    types=toList(trigger['event']),
+                    branches=toList(trigger.get('branch')),
+                    refs=toList(trigger.get('ref')),
+                    event_approvals=approvals,
+                    comments=comments,
+                    emails=emails,
+                    usernames=usernames,
+                    required_any_approval=(
+                        toList(trigger.get('require-approval'))
+                    ),
+                    required_all_approvals=toList(
+                        trigger.get('require-all-approvals')
+                    ),
+                )
+                efilters.append(f)
+
+        return efilters
 
 
 def validate_trigger(trigger_data):

trungminhnguyen/zuul

zuul/trigger/gerrit.py
@@ -16,7 +16,7 @@ import logging
 import threading
 import time
 import voluptuous
-from zuul.model import TriggerEvent
+from zuul.model import EventFilter, TriggerEvent
 from zuul.trigger import BaseTrigger
 
 
@@ -135,6 +135,51 @@ class GerritTrigger(BaseTrigger):
     def stop(self):
         self.gerrit_connector.stop()
         self.gerrit_connector.join()
+
+    def getEventFilters(self, trigger_conf):
+        def toList(item):
+            if not item:
+                return []
+            if isinstance(item, list):
+                return item
+            return [item]
+
+        efilters = []
+        if 'gerrit' in trigger_conf:
+            for trigger in toList(trigger_conf['gerrit']):
+                approvals = {}
+                for approval_dict in toList(trigger.get('approval')):
+                    for k, v in approval_dict.items():
+                        approvals[k] = v
+                # Backwards compat for *_filter versions of these args
+                comments = toList(trigger.get('comment'))
+                if not comments:
+                    comments = toList(trigger.get('comment_filter'))
+                emails = toList(trigger.get('email'))
+                if not emails:
+                    emails = toList(trigger.get('email_filter'))
+                usernames = toList(trigger.get('username'))
+                if not usernames:
+                    usernames = toList(trigger.get('username_filter'))
+                f = EventFilter(
+                    trigger=self,
+                    types=toList(trigger['event']),
+                    branches=toList(trigger.get('branch')),
+                    refs=toList(trigger.get('ref')),
+                    event_approvals=approvals,
+                    comments=comments,
+                    emails=emails,
+                    usernames=usernames,
+                    required_approvals=(
+                        toList(trigger.get('require-approval'))
+                    ),
+                    reject_approvals=toList(
+                        trigger.get('reject-approval')
+                    ),
+                )
+                efilters.append(f)
+
+        return efilters
 
 
 def validate_trigger(trigger_data):
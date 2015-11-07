b1a290b0411ab9ce53494ef8790f4f5aece86982
Jan Hruban <jan.hruban@gooddata.com>
2015-11-07 14:39:07 +0100

    support github pull reqeust labels
    Allow to match trigger on adding or removing github labels. Also
    reporter can add or remove labels.
    
    Change-Id: Id385b92590e252c283ba3ebe1ecfd33b34469a2e

--- zuul/model.py
+++ zuul/model.py
@@ -1025,6 +1025,7 @@ class TriggerEvent(object):
         self.approvals = []
         self.branch = None
         self.comment = None
+        self.label = None
         # ref-updated
         self.ref = None
         self.oldrev = None
@@ -1147,7 +1148,7 @@ class EventFilter(BaseFilter):
     def __init__(self, trigger, types=[], branches=[], refs=[],
                  event_approvals={}, comments=[], emails=[], usernames=[],
                  timespecs=[], required_approvals=[], reject_approvals=[],
-                 pipelines=[], ignore_deletes=True):
+                 pipelines=[], labels=[], ignore_deletes=True):
         super(EventFilter, self).__init__(
             required_approvals=required_approvals,
             reject_approvals=reject_approvals)
@@ -1168,6 +1169,7 @@ class EventFilter(BaseFilter):
         self.pipelines = [re.compile(x) for x in pipelines]
         self.event_approvals = event_approvals
         self.timespecs = timespecs
+        self.labels = labels
         self.ignore_deletes = ignore_deletes
 
     def __repr__(self):
@@ -1200,6 +1202,8 @@ class EventFilter(BaseFilter):
             ret += ' username_filters: %s' % ', '.join(self._usernames)
         if self.timespecs:
             ret += ' timespecs: %s' % ', '.join(self.timespecs)
+        if self.labels:
+            ret += ' labels: %s' % ', '.join(self.labels)
         ret += '>'
 
         return ret
@@ -1294,6 +1298,10 @@ class EventFilter(BaseFilter):
             if (event.timespec == timespec):
                 matches_timespec = True
         if self.timespecs and not matches_timespec:
+            return False
+
+        # labels are ORed
+        if self.labels and event.label not in self.labels:
             return False
 
         return True

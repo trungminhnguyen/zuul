5b54786a31f308a18ec688ed5c7c2c18b85c42a6
Jan Hruban <jan.hruban@gooddata.com>
2015-11-07 14:39:07 +0100

    support github pull reqeust labels
    Allow to match trigger on adding or removing github labels. Also
    reporter can add or remove labels.
    
    Change-Id: Id385b92590e252c283ba3ebe1ecfd33b34469a2e

--- zuul/connection/github.py
+++ zuul/connection/github.py
@@ -112,6 +112,12 @@ class GithubWebhookListener():
             event.type = 'pr-close'
         elif action == 'reopened':
             event.type = 'pr-reopen'
+        elif action == 'labeled':
+            event.type = 'pr-label'
+            event.label = body['label']['name']
+        elif action == 'unlabeled':
+            event.type = 'pr-label'
+            event.label = '-' + body['label']['name']
         else:
             return None
 
@@ -123,6 +129,16 @@ class GithubWebhookListener():
         action = body.get('action')
         if action != 'created':
             return
+        pr_body = self._issue_to_pull_request(body)
+        if pr_body is None:
+            return
+
+        event = self._pull_request_to_event(pr_body)
+        event.comment = body.get('comment').get('body')
+        event.type = 'pr-comment'
+        return event
+
+    def _issue_to_pull_request(self, body):
         number = body.get('issue').get('number')
         project_name = body.get('repository').get('full_name')
         owner, project = project_name.split('/')
@@ -130,12 +146,7 @@ class GithubWebhookListener():
         if pr_body is None:
             self.log.debug('Pull request #%s not found in project %s' %
                            (number, project_name))
-            return
-
-        event = self._pull_request_to_event(pr_body)
-        event.comment = body.get('comment').get('body')
-        event.type = 'pr-comment'
-        return event
+        return pr_body
 
     def _validate_signature(self, request):
         secret = self.connection.connection_config.get('webhook_token', None)
@@ -260,6 +271,14 @@ class GithubConnection(BaseConnection):
                         url='', description='', context=''):
         repository = self.github.repository(owner, project)
         repository.create_status(sha, state, url, description, context)
+
+    def labelPull(self, owner, project, pr_number, label):
+        pull_request = self.github.issue(owner, project, pr_number)
+        pull_request.add_label(label)
+
+    def unlabelPull(self, owner, project, pr_number, label):
+        pull_request = self.github.issue(owner, project, pr_number)
+        pull_request.remove_label(label)
 
 
 def getSchema():
